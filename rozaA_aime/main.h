2e4cc390192818fd1bb99b71e581709549498ee0
VisualCoder123 <yurii.zgoda@mail.ru>
2017-09-16 22:47:26 +0300

    Исправлена главная ошибка анимации, теперь все работает

--- rozaA_aime/main.h
+++ rozaA_aime/main.h
@@ -22,34 +22,33 @@ int cx, cy, bytesRow;
 double Px[4][4];
 double Py[4][4];
 double Pz[4][4];
-
-
-
 //Далее PPx, PPy, PPz - массивы опорных точек трансформации
 //Четверки точек описывают кривые Безье, которые, в свою очередь,
 //определяют точки-ориентиры Pij поверхности Безье
 double PPx[64] =
-	{1,1,1,1,//P00
-	80,80,80,80,    //P01
-	80,80,80,256,    //P02
-	1,100,200,367,    //P03
-	2,2,2,2,    //P10
-	120,120,120,120,   //P11
-	120,120,120,201,//P12
-	2,100,200,300,	//P13
-	2,2,2,2,    //P20
-	120,120,120,120,	//P21
-	120,120,120,189,	//P22
-	2,80,160,262,    //P23
-	1,1,1,1,    //P30
-	80,80,80,80,    //P31
-	80,80,80,174,    //P32
-	1,60,160,240};	//P33
+{ 1,1,1,1,//P00
+80,80,80,80,    //P01
+80,80,80,256,    //P02
+1,100,200,367,    //P03
+2,2,2,2,    //P10
+120,120,120,120,   //P11
+120,120,120,201,//P12
+2,100,200,300,	//P13
+2,2,2,2,    //P20
+120,120,120,120,	//P21
+120,120,120,189,	//P22
+2,80,160,262,    //P23
+1,1,1,1,    //P30
+80,80,80,80,    //P31
+80,80,80,174,    //P32
+1,60,160,240 };	//P33
+
 
 double PPy[64] = {
 	-1, -1, -1, -1,
 	-80, -80, -80, -80,
-	-1, -30, -60, -131,
+	-80, -80, -80, -76,
+	-1,-30,-60,-131,
 	-1, -1, -1, -1,
 	-40, -40, -40, -40,
 	-40, -40, -40, -36,
@@ -59,11 +58,9 @@ double PPy[64] = {
 	40, 40, 40, 37,
 	1, 10, 20, 29,
 	1, 1, 1, 1,
-	8, 80, 80, 80,
+	80, 80, 80, 80,
 	80, 80, 80, 76,
 	1, 10, 20, 87 };
-
-
 double PPz[64] = {
 	0, 0, 0, 0,
 	0, 0, 0, 0,
@@ -82,8 +79,6 @@ double PPz[64] = {
 	150, 150, 150, 89,
 	150, 150, 150, 0 };
 
-
-
 //---- определение коэффициентов преобразования -----
 //---------- для экранных координат -------------
 VOID InitViewTranform(double alpha, double beta, int cx, int cy)
@@ -95,7 +90,6 @@ VOID InitViewTranform(double alpha, double beta, int cx, int cy)
 	Bview = -sin(alpha);
 	Cview = sin(alpha)*cos(beta);
 	Dview = cos(alpha)*cos(beta);
-	Eview = cos(alpha)*cos(beta);
 	Eview = -sin(beta);
 	Fview = sin(alpha)*sin(beta);
 	Gview = cos(alpha)*sin(beta);
@@ -118,7 +112,6 @@ VOID SetPixelZ(int x, int y, float z, COLORREF clr)
 		bmp[k + 1] = GetGValue(clr);
 		bmp[k + 2] = GetRValue(clr);
 	}
-
 }
 
 //------ преобразование мировых координат в экранные ------
@@ -129,11 +122,13 @@ VOID ScreenKoord(VERTEX &v)
 	tmp.y = v.x*Cview + v.y*Dview + v.z*Eview + Yview;
 	tmp.z = v.x*Fview + v.y*Gview + v.z*Hview;
 	v = tmp;
+
 }
 
 //-------- косинус угла нормали для треугольника ----------
 double NormalVectorCos(VERTEX v1, VERTEX v2, VERTEX v3)
 {
+
 	double nx, ny, nz;
 	v2.x -= v1.x;
 	v2.y -= v1.y;
@@ -170,25 +165,24 @@ VOID LineGouraud(int x1, int x2, int y, double z1, double z2, double k1, double 
 	kk = (k2 - k1) / (double)(x2 - x1);
 
 	if (x1 < x2)
-	for (x = x1; x < x2; x++)
-	{
-		z = z1 + (double)(x - x1)*kz;
-		k = k1 + (double)(x - x1)*kk;
-		SetPixelZ(x, y, z, RGB((BYTE)(k*(double)red),
-			(BYTE)(k*(double)gre),
-			(BYTE)(k*(double)blu)));
-	}
+		for (x = x1; x < x2; x++)
+		{
+			z = z1 + (double)(x - x1)*kz;
+			k = k1 + (double)(x - x1)*kk;
+			SetPixelZ(x, y, z, RGB((BYTE)(k*(double)red),
+				(BYTE)(k*(double)gre),
+				(BYTE)(k*(double)blu)));
+		}
 
 	if (x1>x2)
-	for (x = x2; x < x1; x++)
-	{
-		z = z1 + (double)(x - x1)*kz;
-		k = k1 + (double)(x - x1)*kk;
-		SetPixelZ(x, y, z, RGB((BYTE)(k*(double)red),
-			(BYTE)(k*(double)gre),
-			(BYTE)(k*(double)blu)));
-	}
-
+		for (x = x2; x < x1; x++)
+		{
+			z = z1 + (double)(x - x1)*kz;
+			k = k1 + (double)(x - x1)*kk;
+			SetPixelZ(x, y, z, RGB((BYTE)(k*(double)red),
+				(BYTE)(k*(double)gre),
+				(BYTE)(k*(double)blu)));
+		}
 }
 
 //------------ рисование одной полигональной грани -------------
@@ -221,19 +215,19 @@ VOID PolygonGouraud(VERTEX *v, double *kr, int nv, COLORREF clr)
 			y2 = v[en].y;
 
 			if ((y >= y1) && (y<y2) || (y <= y1) && (y>y2))
-			if (y1 != y2)
-			{
-				x1 = v[st].x;
-				z1 = v[st].z;
-				k1 = kr[st];
-				x2 = v[en].x;
-				z2 = v[en].z;
-				k2 = kr[en];
-				x[nhor] = x1 + (x2 - x1)*(double)(y - y1) / (double)(y2 - y1);
-				z[nhor] = z1 + (z2 - z1)*(double)(y - y1) / (double)(y2 - y1);
-				k[nhor] = k1 + (k2 - k1)*(double)(y - y1) / (double)(y2 - y1);
-				nhor++;
-			}
+				if (y1 != y2)
+				{
+					x1 = v[st].x;
+					z1 = v[st].z;
+					k1 = kr[st];
+					x2 = v[en].x;
+					z2 = v[en].z;
+					k2 = kr[en];
+					x[nhor] = x1 + (x2 - x1)*(double)(y - y1) / (double)(y2 - y1);
+					z[nhor] = z1 + (z2 - z1)*(double)(y - y1) / (double)(y2 - y1);
+					k[nhor] = k1 + (k2 - k1)*(double)(y - y1) / (double)(y2 - y1);
+					nhor++;
+				}
 		}
 		if (nhor == 2) LineGouraud(x[0], x[1], y, z[0], z[1], k[0], k[1], clr);
 	}
@@ -249,30 +243,29 @@ VOID DefineSurfacePoints(double t, double ang)
 	ang *= M_PI / 180;
 	A = cos(ang);
 	B = sin(ang);
-	for (i = 0; i < 4;i++)
-	for (j = 0; j < 4; j++)
-	{
-		for (ii = 0; ii < 4; ii++)
+	for (i = 0; i < 4; i++)
+		for (j = 0; j < 4; j++)
 		{
-			index = i * 16 + j * 4 + ii;
-			tmpX[ii] = A*PPx[index] + B*PPy[index];
-			tmpY[ii] = -B*PPx[index] + A*PPy[index];
-			tmpZ[ii] = PPz[index];
-		}
-		for (jj = 3; jj>0; jj--)
-		for (ii = 0; ii < j; ii++)
-		{
-			tmpX[ii] = tmpX[ii] + t*(tmpX[ii + 1] - tmpX[ii]);
-			tmpY[ii] = tmpY[ii] + t*(tmpY[ii + 1] - tmpY[ii]);
-			tmpY[ii] = tmpZ[ii] + t*(tmpZ[ii + 1] - tmpZ[ii]);
-		}
+			for (ii = 0; ii < 4; ii++)
+			{
+				index = i * 16 + j * 4 + ii;
+				tmpX[ii] = A*PPx[index] + B*PPy[index];
+				tmpY[ii] = -B*PPx[index] + A*PPy[index];
+				tmpZ[ii] = PPz[index];
+			}
+			for (jj = 3; jj > 0; jj--)
+				for (ii = 0; ii < jj; ii++)
+				{
+					tmpX[ii] = tmpX[ii] + t*(tmpX[ii + 1] - tmpX[ii]);
+					tmpY[ii] = tmpY[ii] + t*(tmpY[ii + 1] - tmpY[ii]);
+					tmpZ[ii] = tmpZ[ii] + t*(tmpZ[ii + 1] - tmpZ[ii]);
+				}
 
-		Px[i][j] = zoomView*tmpX[0];
-		Py[i][j] = zoomView*tmpY[0];
-		Pz[i][j] = zoomView*tmpZ[0];
-	}
+			Px[i][j] = zoomView*tmpX[0];
+			Py[i][j] = zoomView*tmpY[0];
+			Pz[i][j] = zoomView*tmpZ[0];
+		}
 }
-
 //вычисление координат точки кубической поверхности Безье
 VOID BezierPoint(VERTEX *v, double s, double t)
 {
@@ -284,7 +277,7 @@ VOID BezierPoint(VERTEX *v, double s, double t)
 	ti[0] = 1;
 	ssi[3] = 1;
 	tti[3] = 1;
-	
+
 	for (i = 1; i < 4; i++)
 	{
 		si[i] = s*si[i - 1];
@@ -304,14 +297,14 @@ VOID BezierPoint(VERTEX *v, double s, double t)
 	v->z = 0;
 
 
-	for (i = 0; i <= 3;i++)
-	for (j = 0; j <= 3; j++)
-	{
-		k = si[i] * ti[j];
-		v->x += k*Px[i][j];
-		v->y += k*Py[i][j];
-		v->z += k*Pz[i][j];
-	}
+	for (i = 0; i <= 3; i++)
+		for (j = 0; j <= 3; j++)
+		{
+			k = si[i] * ti[j];
+			v->x += k*Px[i][j];
+			v->y += k*Py[i][j];
+			v->z += k*Pz[i][j];
+		}
 }
 
 VOID DrawBezierSurface(COLORREF clr)
@@ -324,64 +317,65 @@ VOID DrawBezierSurface(COLORREF clr)
 	double kr[11][11];
 	double kn;
 
-	for (i = 0; i < 11;i++)
-	for (j = 0; j < 11; j++)
-	{
-		BezierPoint(&v, (double)i / 10, (double)j / 10);
-		ScreenKoord(v);
-		vb[i][j] = v;
-	}
-
-	for (i = 0; i < 11;i++)
-	for (j = 0; j < 11; j++)
-	{
-		num = 0;
-		kn = 0;
-		if ((i < 10) && (j < 10))
+	for (i = 0; i < 11; i++)
+		for (j = 0; j < 11; j++)
 		{
-			kn += NormalVectorCos(vb[i][j], vb[i + 1][j], vb[i][j + 1]);
-			num++;
+			BezierPoint(&v, (double)i / 10, (double)j / 10);
+			ScreenKoord(v);
+			vb[i][j] = v;
 		}
 
-		if ((i<10) && (j>0))
+	for (i = 0; i < 11; i++)
+		for (j = 0; j < 11; j++)
 		{
-			kn += NormalVectorCos(vb[i][j], vb[i + 1][j], vb[i][j - 1]);
-			num++;
+
+			num = 0;
+			kn = 0;
+			if ((i < 10) && (j < 10))
+			{
+				kn += NormalVectorCos(vb[i][j], vb[i + 1][j], vb[i][j + 1]);
+				num++;
+			}
+
+			if ((i<10) && (j>0))
+			{
+				kn += NormalVectorCos(vb[i][j], vb[i + 1][j], vb[i][j - 1]);
+				num++;
+			}
+
+			if ((i > 0) && (j > 0))
+			{
+				kn += NormalVectorCos(vb[i][j], vb[i - 1][j], vb[i][j - 1]);
+				num++;
+			}
+			if ((i > 0) && (j < 10))
+			{
+				kn += NormalVectorCos(vb[i][j], vb[i - 1][j], vb[i][j + 1]);
+				num++;
+			}
+			kr[i][j] = kn / (double)num;
 		}
 
-		if ((i > 0) && (j > 0))
+	for (i = 0; i < 10; i++)
+		for (j = 0; j < 10; j++)
 		{
-			kn += NormalVectorCos(vb[i][j], vb[i - 1][j], vb[i][j - 1]);
-			num++;
-		}
-		if ((i > 0) && (j < 10))
-		{
-			kn += NormalVectorCos(vb[i][j], vb[i - 1][j], vb[i][j + 1]);
-			num++;
-		}
-		kr[i][j] =  kn / (double)num;
-	}
+			gran[0] = vb[i][j];
+			gran[1] = vb[i + 1][j];
+			gran[2] = vb[i + 1][j + 1];
+			gran[3] = vb[i][j + 1];
+			kgran[0] = kr[i][j];
+			kgran[1] = kr[i + 1][j];
+			kgran[2] = kr[i + 1][j + 1];
+			kgran[3] = kr[i][j + 1];
 
-	for (i = 0; i < 10;i++)
-	for (j = 0; j < 10; j++)
-	{
-		gran[0] = vb[i][j];
-		gran[1] = vb[i + 1][j];
-		gran[2] = vb[i + 1][j + 1];
-		gran[3] = vb[i][j + 1];
-		kgran[0] = kr[i][j];
-		kgran[1] = kr[i + 1][j];
-		kgran[2] = kr[i + 1][j + 1];
-		kgran[3] = kr[i][j + 1];
-
-		PolygonGouraud(gran, kgran, 4, clr);
-	}
+			PolygonGouraud(gran, kgran, 4, clr);
+		}
 }
-
 
 
 BOOL InitBMP()
 {
+
 	int bmWidth;
 
 	bmpinfo = new BITMAPINFO[sizeof(BITMAPINFO)];
@@ -401,7 +395,6 @@ BOOL InitBMP()
 		delete[]bmpinfo;
 		return FALSE;
 	}
-
 }
 
 
